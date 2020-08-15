e03be187ba7257ec18734ea34711348ebb7317ca
singhgurpreetcs <er.gurpreet619@yahoo.com>
2020-08-15 08:50:43 +0530

    Spring autowire byType in XML Configuration Demo

---
+++ springcoreadvancedAutoWiringByType/springcoreadvanced/src/main/java/com/gurpreet/spring/springcoreadvanced/autowiring/Employee.java
@@ -0,0 +1,18 @@
+package com.gurpreet.spring.springcoreadvanced.autowiring;
+
+public class Employee {
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
