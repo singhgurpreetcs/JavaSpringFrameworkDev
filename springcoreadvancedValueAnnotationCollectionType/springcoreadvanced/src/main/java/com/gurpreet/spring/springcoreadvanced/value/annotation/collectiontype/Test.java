4271f94a4b43f8b8ecc9da2514b8b73f82770cc3
singhgurpreetcs <er.gurpreet619@yahoo.com>
2020-08-15 09:00:07 +0530

    Java Spring Collection Type implementation using Stereotype Annotations i.e. Value

---
+++ springcoreadvancedValueAnnotationCollectionType/springcoreadvanced/src/main/java/com/gurpreet/spring/springcoreadvanced/value/annotation/collectiontype/Test.java
@@ -0,0 +1,20 @@
+package com.gurpreet.spring.springcoreadvanced.value.annotation.collectiontype;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"com/gurpreet/spring/springcoreadvanced/value/annotation/collectiontype/config.xml");
+		// use instrutor in camel case because spring will create an object and read the bean from the container
+		Instructor e = (Instructor) ctx.getBean("instructor");
+		System.out.println(e);
+		
+		//object2
+		Instructor e2 = (Instructor) ctx.getBean("instructor");
+		System.out.println(e2);
+	}
+
+}
