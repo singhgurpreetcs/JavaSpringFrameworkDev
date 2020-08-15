65f0607460f04764a45336b012ae529648a4c861
singhgurpreetcs <er.gurpreet619@yahoo.com>
2020-08-15 08:53:03 +0530

    Spring autowire constructor in XML Configuration Demo

---
+++ springcoreadvancedAutoWiringByConstructor/springcoreadvanced/src/main/java/com/gurpreet/spring/springcoreadvanced/autowiring/Employee.java
@@ -0,0 +1,22 @@
+package com.gurpreet.spring.springcoreadvanced.autowiring;
+
+public class Employee {
+	private Address address;
+
+	Employee(Address address) {
+		this.address = address;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
