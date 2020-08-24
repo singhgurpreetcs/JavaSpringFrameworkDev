3ce0d0b875d9126fe2bf7a72ea1635c2eee3218a
singhgurpreetcs <er.gurpreet619@yahoo.com>
2020-08-24 07:19:27 +0530

    Java Spring configuration by spreading configuration in various classes and importing it in another where we need

---
+++ springjavaconfigSpreadConfigurationAcrossBeans_ImportAnnotation/Readme.txt
@@ -0,0 +1,2 @@
+We can spread java configuration across beans and import into another bean
+using the @Import annotation.
---
+++ springjavaconfigSpreadConfigurationAcrossBeans_ImportAnnotation/springjavaconfig/src/main/java/com/gurpreet/spring/springjavaconfig/Dao.java
@@ -0,0 +1,11 @@
+package com.gurpreet.spring.springjavaconfig;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dao {
+	public void create()
+	{
+		System.out.println("created");
+	}
+}
